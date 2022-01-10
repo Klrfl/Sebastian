a89b0c9c777ac9f73c6a679f2e18b9b4bab3dd54
Efraim Munthe <efrayanglain@gmail.com>
2022-01-10 19:10:56 +0700

    fixed sidebar

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,10 @@
 //an empty js file
 let openSidebar = document.getElementById('openSidebar');
 let sidebar = document.getElementById('sidebar');
+let burgerSpan = document.getElementsByClassName('span');
+
 
 openSidebar.addEventListener('click', ()=>{
   sidebar.classList.toggle('slide');
-})
+  burgerSpan.classList.toggle('slide');
+});
