d3ba34c449d3f658f531a65b7de223e754e56115
Efraim Munthe <efrayanglain@gmail.com>
2022-01-10 19:05:50 +0700

    added sidebar

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
 //an empty js file
+let openSidebar = document.getElementById('openSidebar');
+let sidebar = document.getElementById('sidebar');
+
+openSidebar.addEventListener('click', ()=>{
+  sidebar.classList.toggle('slide');
+})
